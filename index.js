74864a5dc1182ad7d0f3c03ce5971c16ef09d8f9
Navpreet Virk <navpreetvirk96@gmail.com>
2022-08-22 21:55:14 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+// MongoDB
+const { MongoClient, ObjectId } = require("mongodb")
+const uri = 'mongodb+srv://root:root@cluster0.jtbuagh.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri)
+const coll = client.db('assignment03').collection('notes')
+
+async function createNote(noteData) {
+    try {
+        await client.connect()
+        await coll.insertOne({
+            content: noteData.content,
+            title: noteData.title
+        })
+    } finally {
+        client.close()
+    }
+}
+
+async function deleteNote(noteData) {
+    try {
+        await client.connect()
+        await coll.deleteOne({ _id: new ObjectId(noteData.id) })
+    } finally {
+        client.close()
+    }
+}
+
+async function getNotes() {
+    try {
+        await client.connect()
+        let notes = []
+        const response = await coll.find({})
+        await response.forEach(note => notes.push(note))
+        return notes
+    } finally {
+        client.close()
+    }
+}
+
+async function updateNote(noteData) {
+    try {
+        await client.connect()
+        const filter = {
+            _id: new ObjectId(noteData.id)
+        }
+        const update = {
+            $set: {
+                content: noteData.content,
+                title: noteData.title
+            }
+        }
+        await coll.updateOne(filter, update, { upsert: false })
+    } finally {
+        client.close()
+    }
+}
+
+// Express
+const express = require('express')
+
+const app = express()
+app.use(express.static('static'))
+app.use(express.json())
+express.urlencoded({extended: false})
+app.listen(80)
+
+app.get('/api/notes', async (req, res) => {
+    res.json(await getNotes())
+})
+
+app.delete('/api/note', async (req, res) => {
+    await deleteNote(req.body)
+    res.json({message: 'Delete Successful'})
+})
+
+app.post('/api/note', async (req, res) => {
+    await createNote(req.body)
+    res.json({message: 'Creation Successful'})
+})
+
+app.put('/api/note', async (req, res) => {
+    await updateNote(req.body)
+    res.json({message: 'Update Successful'})
+})
---
+++ static/script.js
@@ -0,0 +1,110 @@
+const API_URL = 'http://localhost/api'
+const converter = new showdown.Converter()
+
+async function confirmDelete(noteID) {
+    await fetch(`${API_URL}/note`, {
+        body: JSON.stringify({
+            id: noteID
+        }),
+        headers: {'Content-Type': 'application/json'},
+        method: 'DELETE'
+    })
+    .then(r => location.reload())
+}
+
+async function createNote() {
+    await fetch(`${API_URL}/note`, {
+        body: JSON.stringify({
+            content: document.querySelector('#new-note-content').value,
+            title: document.querySelector('#new-note-title').value
+        }),
+        headers: {'Content-Type': 'application/json'},
+        method: 'POST'
+    })
+    .then(r => location.reload())
+}
+
+function deleteNote(note) {
+    const id = note.attributes['data-note-id'].value
+    document.querySelector('#delete-button').setAttribute('onclick', `confirmDelete('${id}')`)
+}
+
+function editNote(note) {
+    const content = note.attributes['data-note-content'].value
+    const id = note.attributes['data-note-id'].value
+    const title = note.attributes['data-note-title'].value
+    document.querySelector('#edit-note-content').value = content
+    document.querySelector('#edit-note-title').value = title
+    document.querySelector('#update-note-button').setAttribute('onclick', `updateNote('${id}')`)
+}
+
+function fetchNotes() {
+    fetch(`${API_URL}/notes`)
+        .then(r => r.json())
+        .then(d => renderNotes(d))
+}
+
+function renderNotes(notes) {
+    if (notes.length > 0) {
+        document.querySelector('#no-notes-alert').classList.add('d-none')
+        notes.forEach(note => {
+            const noteHTML =
+            `<div class="align-items-center bg-dark border-secondary d-flex justify-content-between list-group-item p-4 text-light" data-bs-target="#view-note-modal" data-bs-toggle="modal" role="button" data-note-content="${note.content}" data-note-id="${note._id}" data-note-title="${note.title}" onclick="viewNote(this)">
+                <h5 class="mb-0">${note.title}</h5>
+                <div class="btn-group">
+                    <button class="btn btn-lg btn-warning" data-bs-target="#edit-note-modal" data-bs-toggle="modal" onclick="editNote(this.parentNode.parentNode)">
+                        <i class="bi bi-pencil-square"></i>
+                    </button>
+                    <button class="btn btn-lg btn-danger" data-bs-target="#delete-note-modal"
+                        data-bs-toggle="modal" onclick="deleteNote(this.parentNode.parentNode)">
+                        <i class="bi bi-trash-fill"></i>
+                    </button>
+                </div>
+            </div>`
+
+            document.querySelector('#note-list').innerHTML += noteHTML
+        })
+    }
+}
+
+async function updateNote(noteID) {
+    await fetch(`${API_URL}/note`, {
+        body: JSON.stringify({
+            content: document.querySelector('#edit-note-content').value,
+            id: noteID,
+            title: document.querySelector('#edit-note-title').value
+        }),
+        headers: {'Content-Type': 'application/json'},
+        method: 'PUT'
+    })
+    .then(r => location.reload())
+}
+
+function viewNote(note) {
+    const content = note.attributes['data-note-content'].value
+    const title = note.attributes['data-note-title'].value
+    document.querySelector('#view-note-modal .modal-body').innerHTML = converter.makeHtml(content)
+    document.querySelector('#view-note-modal .modal-title').innerHTML = title
+}
+
+fetchNotes()
+
+// Form Validation
+
+function validate(input) {
+    let enableButton = true
+    if (input.value == '') {
+        input.classList.add('border-danger')
+        input.classList.add('is-invalid')
+        input.classList.remove('border-success')
+        input.classList.remove('is-valid')
+    } else {
+        input.classList.add('is-valid')
+        input.classList.add('border-success')
+        input.classList.remove('is-invalid')
+        input.classList.remove('border-danger')
+    }
+    input.parentNode.querySelectorAll('input, textarea').forEach(input => (input.value == '') ? enableButton = false : null)
+    if (enableButton) input.parentNode.querySelector('button').removeAttribute('disabled')
+    else input.parentNode.querySelector('button').setAttribute('disabled', '')
+}
